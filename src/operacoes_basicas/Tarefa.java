b409dd407d337d46a95d9e1f27362cc3731b2335
unknown <philippediniz@outlook.com>
2024-05-17 12:30:19 -0300

    Operações básicas List: Testando os métodos

--- src/operacoes_basicas/Tarefa.java
+++ src/operacoes_basicas/Tarefa.java
@@ -14,4 +14,10 @@ public class Tarefa {
     public String getDescricao() {
         return descricao;
     }
+
+    @Override
+    public String toString() {
+        return descricao ;
+
+    }
 }

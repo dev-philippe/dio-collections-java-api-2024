5972c7bab35e938a9b3db78a8a7a566b7fee6eff
unknown <philippediniz@outlook.com>
2024-05-17 11:32:55 -0300

    Operações básicas list

---
+++ src/operacoes_basicas/Tarefa.java
@@ -0,0 +1,17 @@
+package operacoes_basicas;
+
+public class Tarefa {
+
+    // atributo
+
+    private String descricao;
+
+    public  Tarefa(String descricao) {
+        this.descricao = descricao;
+
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}

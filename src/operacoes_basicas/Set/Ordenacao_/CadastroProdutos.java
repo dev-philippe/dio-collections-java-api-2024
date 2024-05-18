137a6544b23a33eafdee584967a5e1aed69b2f0d
unknown <philippediniz@outlook.com>
2024-05-18 14:53:07 -0300

    Ordenação Set: Testando os métodos

--- src/operacoes_basicas/Set/Ordenacao_/CadastroProdutos.java
+++ src/operacoes_basicas/Set/Ordenacao_/CadastroProdutos.java
@@ -26,11 +26,25 @@ public class CadastroProdutos {
 
     }
 
-    public Set<Produto exibirPorPreco(){
+    public Set<Produto> exibirPorPreco(){
         Set<Produto> produtosPorPreco = new TreeSet<>();
         produtoSet.addAll(produtoSet);
         return produtosPorPreco;
 
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L,"Produto 5",15d,5);
+        cadastroProdutos.adicionarProduto(2L,"Produto 0",20d,10);
+        cadastroProdutos.adicionarProduto(1L,"Produto 3",10d,2);
+        cadastroProdutos.adicionarProduto(9L,"Produto 9",2d,2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+      System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirPorPreco());
     }
 
 

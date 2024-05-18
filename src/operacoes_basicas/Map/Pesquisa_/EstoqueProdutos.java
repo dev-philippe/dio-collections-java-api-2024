415fdbf174148ef7dcb61bfafb61431b555a5171
unknown <philippediniz@outlook.com>
2024-05-18 17:38:02 -0300

    Pesquisa Map: Testando Os Métodos

--- src/operacoes_basicas/Map/Pesquisa_/EstoqueProdutos.java
+++ src/operacoes_basicas/Map/Pesquisa_/EstoqueProdutos.java
@@ -14,7 +14,7 @@ public class EstoqueProdutos {
 
     }
 
-    public void adicionarProduto(Long codigo, String nome, double preco, int quantidade){
+    public void adicionarProduto(Long codigo, String nome, int quantidade, double preco){
         estoqueProdutosMap.put(codigo, new Produto(nome,preco,quantidade));
     }
 
@@ -48,4 +48,36 @@ public class EstoqueProdutos {
         }
         return produtoMaisCaro;
     }
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+
+        // Exibe o estoque vazio
+        estoque.exibirProdutos();
+
+        // Adiciona produtos ao estoque
+        estoque.adicionarProduto(1L, "Notebook", 1, 1500.0);
+        estoque.adicionarProduto(2L, "Mouse", 5, 25.0);
+        estoque.adicionarProduto(3L, "Monitor", 10, 400.0);
+        estoque.adicionarProduto(4L, "Teclado", 2, 40.0);
+
+        // Exibe os produtos no estoque
+        estoque.exibirProdutos();
+
+        // Calcula e exibe o valor total do estoque
+        System.out.println("Valor total do estoque: R$" + estoque.calculaValorTotalEstoque());
+
+        // Obtém e exibe o produto mais caro
+        Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto mais caro: " + produtoMaisCaro);
+
+        // Obtém e exibe o produto mais barato
+        Produto produtoMaisBarato = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto mais barato: " + produtoMaisBarato);
+
+        // Obtém e exibe o produto com a maior quantidade em valor no estoque
+        Produto produtoMaiorQuantidadeValorTotal = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto com maior quantidade em valor no estoque: " + produtoMaiorQuantidadeValorTotal);
+    }
 }
+
+

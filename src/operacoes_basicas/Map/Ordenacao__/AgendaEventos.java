3d3e9d98f88fc8351aaebc7b9f61313a072e868f
unknown <philippediniz@outlook.com>
2024-05-20 09:29:43 -0300

    Ordenação Map : Testando os métodos

--- src/operacoes_basicas/Map/Ordenacao__/AgendaEventos.java
+++ src/operacoes_basicas/Map/Ordenacao__/AgendaEventos.java
@@ -1,9 +1,7 @@
 package operacoes_basicas.Map.Ordenacao__;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AgendaEventos {
 
@@ -17,18 +15,42 @@ public class AgendaEventos {
 
     public void adicionarEvento(LocalDate data, String nome, String atracao) {
         Evento evento = new Evento(nome, atracao);
-        eventosMap.put(data,new Evento(nome,atracao));
+        eventosMap.put(data, new Evento(nome, atracao));
 
 
     }
 
-    public  void exibirAgenda(){
+    public void exibirAgenda() {
         Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(eventosMap);
+
         System.out.println(eventoTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        // Set<LocalDate> dataSet = eventosMap.keySet();
+        // Collection<Evento> values = eventosMap.values();
+        //  eventosMap.get();
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+
+        }
+
     }
 
     public static void main(String[] args) {
         AgendaEventos agendaEventos = new AgendaEventos();
+
+        System.out.println(LocalDate.now());
 
     }
 }
